0fdf602b5f469d9cf4bb55a400b4ccb8b2bf5d7b
Chris Miller <cmiller@genome.wustl.edu>
2013-07-15 17:41:07 -0500

    version 1.5.4

--- copyCat/R/zzz.R
+++ copyCat/R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   if( !require(methods) ) stop("we require methods for package copyCat")
   initRdClass()
-  print("Using copyCat version 1.5.3")
+  print("Using copyCat version 1.5.4")
 }
