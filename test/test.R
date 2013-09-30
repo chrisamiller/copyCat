6a2f934ceedfda218825a4087236b639d67347d8
Chris Miller <cmiller@genome.wustl.edu>
2013-09-30 17:52:00 -0500

    committing test data

---
+++ test/test.R
@@ -0,0 +1,18 @@
+library(copyCat)
+#do a paired sample analysis on the test data
+runPairedSampleAnalysis(annotationDirectory="/gscmnt/gc6122/info/medseq/annotations/copyCat/hg19.chr14only",
+                        outputDirectory="results",
+                        normal="data/normal.wind",
+                        tumor="data/tumor.wind",
+                        inputType="bins",
+                        maxCores=4,
+                        binSize=0,
+                        perLibrary=1,
+                        perReadLength=1,
+                        verbose=TRUE,
+                        dumpBins=TRUE,
+                        doGcCorrection=TRUE,
+                        normalSamtoolsFile="data/normal.samtools",
+                        tumorSamtoolsFile="data/tumor.samtools",
+                        purity=0.95)
+print("test completed");
