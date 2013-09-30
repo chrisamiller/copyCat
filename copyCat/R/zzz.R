8f55084b1aec1ad0289f222599bd691dc44f4ab7
Chris Miller <cmiller@genome.wustl.edu>
2013-10-01 00:35:22 +0200

    minor tweaks to fix build warnings

--- copyCat/R/zzz.R
+++ copyCat/R/zzz.R
@@ -1,4 +1,23 @@
 .onLoad <- function(libname, pkgname) {
+
+  ## silly hack to get around a catch-22 in the namespace code
+  ##
+  ## loading the methods package with library(methods) (on R 2.15)
+  ## produces the NOTE: 
+  ##
+  ## Package startup functions should not change the search path
+  ##
+  ## removing the loading of methods causes a different error:
+  ##    Error: .onLoad failed in loadNamespace() for 'copyCat', details:
+  ##    call: initRdClass()
+  ##    error: could not find function "setClass"
+  ##    Execution halted
+  ##
+  ## so we disguise the call (based on code in r-bioc-iranges)
+
+  sillyname <- library
+  sillyname(methods)
+
   initRdClass()
   packageStartupMessage("Using copyCat version 1.6.0")
 }
